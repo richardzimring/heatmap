8bf721aefaa74496f5a5e427ca50277c5788041e
Richard Zimring <zimringrichard@gmail.com>
2023-06-02 16:13:21 -0500

    added dynamodb caching

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,5 +1,5 @@
-// const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-// const { DynamoDBDocumentClient, GetCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
@@ -9,9 +9,9 @@ import { fetchOptionData } from "./requests/getOptions";
 import { fetchQuote } from "./requests/getQuote";
 import { stringifyDates } from "./utils";
 
-// const USERS_TABLE = process.env.USERS_TABLE;
-// const client = new DynamoDBClient();
-// const dynamoDbClient = DynamoDBDocumentClient.from(client);
+const OPTIONS_TABLE = process.env.OPTIONS_TABLE;
+const client = new DynamoDBClient();
+const ddbDocClient = DynamoDBDocumentClient.from(client);
 
 app.use(express.json());
 
@@ -20,10 +20,24 @@ app.get("/data/:ticker", async function (req, res) {
 
   const ticker = req.params.ticker;
 
-  // TODO: check if ticker has been cached with an `accessed_date` within the last TRADING hour
-  // including invalid tickers
-  // TODO: implement dynamoDB caching
+  // check if ticker has been cached in DynamoDB 
+  const { Item } = await ddbDocClient.send(
+    new GetCommand({
+      TableName: OPTIONS_TABLE,
+      Key: {
+        ticker: ticker,
+      },
+    })
+  );
 
+  // if data exists in dynamoDB and date is within the last hour, return cached data
+  if (Item && new Date(Item.updated_at).getTime() > Date.now() - 1000 * 60 * 60) {
+    console.log("using data from dynamoDB cache")
+    res.json(Item);
+    return;
+  }
+  
+  console.log("cached data either not found or expired. fetching data from Tradier...")
 
   try {
     // get expiration dates
@@ -42,67 +56,42 @@ app.get("/data/:ticker", async function (req, res) {
     // get options chains
     const optionData = await fetchOptionData(ticker, parseFloat(stockData.price), expirationDates, expirationDatesStringified);
 
-    res.json({
+    const response = {
       ...stockData,
       expirationDates,
       expirationDatesStringified,
       ...optionData
-    });
+    };
+    res.json(response);
+
+    // save response to dynamoDB cache
+    console.log("saving to dynamoDB");
+    await ddbDocClient.send(
+      new PutCommand({
+        TableName: OPTIONS_TABLE,
+        Item: response,
+      })
+    );
 
   } catch (error) {
-    console.log(error);
-    res.status(400).json({ message: error.message });
+    const errorResponse = {
+      ticker: ticker,
+      updated_at: new Date().toISOString(),
+      message: error.message
+    };
+
+    res.json(errorResponse);
+    
+    // save error response to dynamoDB cache
+    console.log("saving to dynamoDB");
+    await ddbDocClient.send(
+      new PutCommand({
+        TableName: OPTIONS_TABLE,
+        Item: errorResponse,
+      })
+    );
   }
 });
-
-// app.get("/users/:userId", async function (req, res) {
-//   const params = {
-//     TableName: USERS_TABLE,
-//     Key: {
-//       userId: req.params.userId,
-//     },
-//   };
-
-//   try {
-//     const { Item } = await dynamoDbClient.send(new GetCommand(params));
-//     if (Item) {
-//       const { userId, name } = Item;
-//       res.json({ userId, name });
-//     } else {
-//       res
-//         .status(404)
-//         .json({ error: 'Could not find user with provided "userId"' });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Could not retreive user" });
-//   }
-// });
-
-// app.post("/users", async function (req, res) {
-//   const { userId, name } = req.body;
-//   if (typeof userId !== "string") {
-//     res.status(400).json({ error: '"userId" must be a string' });
-//   } else if (typeof name !== "string") {
-//     res.status(400).json({ error: '"name" must be a string' });
-//   }
-
-//   const params = {
-//     TableName: USERS_TABLE,
-//     Item: {
-//       userId: userId,
-//       name: name,
-//     },
-//   };
-
-//   try {
-//     await dynamoDbClient.send(new PutCommand(params));
-//     res.json({ userId, name });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Could not create user" });
-//   }
-// });
 
 app.use((req, res, next) => {
   return res.status(404).json({
