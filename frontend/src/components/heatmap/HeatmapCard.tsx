fa2ac0cb649102ce8de5d1ac8a1770f8a665a60d
Richard Zimring <zimringrichard@gmail.com>
2026-02-07 00:41:12 -0600

    improve skeleton styling on mobile

--- frontend/src/components/heatmap/HeatmapCard.tsx
+++ frontend/src/components/heatmap/HeatmapCard.tsx
@@ -25,22 +25,22 @@ function LoadingState() {
       {/* Header skeleton - matches loaded header layout */}
       <div className="flex flex-col sm:flex-row sm:items-start justify-between gap-4 shrink-0">
         {/* Left side - Stock info skeleton */}
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 w-full sm:w-auto">
           {/* Ticker */}
-          <Skeleton className="h-4 w-12 rounded" />
+          <Skeleton className="h-4 w-full sm:w-12 rounded" />
           {/* Description */}
-          <Skeleton className="h-8 w-48 rounded" />
+          <Skeleton className="h-8 w-full sm:w-48 rounded" />
           {/* Price + change row */}
           <div className="flex items-baseline gap-2 mt-1">
-            <Skeleton className="h-8 w-28 rounded" />
-            <Skeleton className="h-5 w-32 rounded" />
+            <Skeleton className="h-8 w-full sm:w-28 rounded" />
+            <Skeleton className="h-5 w-full sm:w-32 rounded" />
           </div>
         </div>
 
         {/* Right side - Controls skeleton */}
         <div className="flex gap-2 w-full sm:w-auto sm:self-end">
-          <Skeleton className="h-9 w-[120px] rounded-md" />
-          <Skeleton className="h-9 w-[120px] rounded-md" />
+          <Skeleton className="h-9 flex-1 sm:flex-none sm:w-[120px] rounded-md" />
+          <Skeleton className="h-9 flex-1 sm:flex-none sm:w-[120px] rounded-md" />
         </div>
       </div>
 
